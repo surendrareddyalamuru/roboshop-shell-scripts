b73fbde07a03e915913dd52b7c094b76cb771349
surendra reddy <surendrareddylamuru01@gmail.com>
2022-10-24 11:52:08 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -2,11 +2,14 @@ LOG_FILE=/tmp/mongodb
 
 echo 'settingup mondodb repo file'
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
+echo status = $?
 
 echo 'installing mongodb server'
 yum install -y mongodb-org &>>$LOG_FILE
+echo status = $?
 
 echo 'starting mongodb service'
 systemctl enable mongod &>>$LOG_FILE
 systemctl start mongod &>>$LOG_FILE
 systemctl restart mongod  &>>$LOG_FILE
+echo status = $?
