243949eabe0a513caf774f0c114df18cad39c114
surendra reddy <surendrareddylamuru01@gmail.com>
2022-10-24 12:33:10 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -8,6 +8,10 @@ echo 'installing mongodb server'
 yum install -y mongodb-org &>>$LOG_FILE
 echo status = $?
 
+echo 'update mongodb listen address'
+sed -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo status = $?
+
 echo 'starting mongodb service'
 systemctl enable mongod &>>$LOG_FILE
 systemctl start mongod &>>$LOG_FILE
