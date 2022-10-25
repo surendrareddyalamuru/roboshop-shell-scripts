3829dc3828745c51473f93761285be3908064adb
surendra reddy <surendrareddylamuru01@gmail.com>
2022-10-25 15:37:46 +0530

    conditions in project.

--- catalogue.sh
+++ catalogue.sh
@@ -6,68 +6,46 @@ if [ $ID -ne 0 ]; then
   exit 1
 fi
 
+statuscheck() {
+  if [ $? -eq 0 ]; then
+   echo -e status = "\e[32mSUCCESSS\e[0m"
+  else
+    echo -e status = "\e[31mFAILURE\e[0m"
+    exit 1
+  f
+}
+
 echo 'setup nodejs repos'
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
-if [ $? -eq 0 ]; then
- echo -e status = "\e[32mSUCCESSS\e[0m"
-else
-  echo -e status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+statuscheck $?
 
 echo 'install nodejs'
 yum install nodejs -y &>>$LOG_FILE
-if [ $? -eq 0 ]; then
- echo -e status = "\e[32mSUCCESSS\e[0m"
-else
-  echo -e status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+statuscheck $?
 
 
 id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
  echo 'Add Roboshop Application User'
  useradd roboshop &>>$LOG_FILE
- if [ $? -eq 0 ]; then
-  echo -e status = "\e[32mSUCCESSS\e[0m"
- else
-   echo -e status = "\e[31mFAILURE\e[0m"
-   exit 1
- fi
-
+statuscheck $?
 fi
 
 echo 'Download Catalogue Application Code'
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
-if [ $? -eq 0 ]; then
- echo -e status = "\e[32mSUCCESSS\e[0m"
-else
-  echo -e status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+statuscheck $?
 
 
 cd /home/roboshop
 
 echo "clean old app content"
 rm -rf catalogue &>>$LOG_FILE
-if [ $? -eq 0 ]; then
- echo -e status = "\e[32mSUCCESSS\e[0m"
-else
-  echo -e status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+statuscheck $?
 
 
 echo 'Extract Catalogue Application Code'
 unzip /tmp/catalogue.zip &>>$LOG_FILE
-if [ $? -eq 0 ]; then
- echo -e status = "\e[32mSUCCESSS\e[0m"
-else
-  echo -e status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+statuscheck $?
 
 
 
@@ -76,22 +54,12 @@ cd /home/roboshop/catalogue
 
 echo 'install Nodejs Dependencies'
 npm install &>>$LOG_FILE
-if [ $? -eq 0 ]; then
- echo -e status = "\e[32mSUCCESSS\e[0m"
-else
-  echo -e status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+statuscheck $?
 
 
 echo 'setup catalogue service'
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
-if [ $? -eq 0 ]; then
- echo -e status = "\e[32mSUCCESSS\e[0m"
-else
-  echo -e status = "\e[31mFAILURE\e[0m"
-  exit 1
-fi
+statuscheck $?
 
 
 
