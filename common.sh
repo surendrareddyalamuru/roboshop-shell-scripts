4ddda22978e39fea3de013eee209d9a300a23215
surendra reddy <surendrareddylamuru01@gmail.com>
2022-10-26 10:38:44 +0530

    user

--- common.sh
+++ common.sh
@@ -12,3 +12,64 @@ statuscheck() {
     exit 1
   fi
 }
+
+Nodejs() {
+echo "setup nodejs repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
+statuscheck $?
+
+
+echo "install nodejs"
+yum install nodejs -y  &>>$LOG_FILE
+statuscheck $?
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+ echo 'Add Roboshop Application User'
+ useradd roboshop &>>$LOG_FILE
+statuscheck $?
+fi
+
+
+echo 'download ${COMPONENT} Application Code'
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG_FILE
+statuscheck $?
+
+
+cd /home/roboshop
+
+echo "clean old app content"
+rm -rf ${COMPONENT} &>>$LOG_FILE
+statuscheck $?
+
+
+echo 'Extract ${COMPONENT} Application Code'
+unzip /tmp/${COMPONENT}.zip &>>$LOG_FILE
+statuscheck $?
+
+
+
+mv ${COMPONENT}-main ${COMPONENT}
+cd /home/roboshop/${COMPONENT}
+
+echo 'install Nodejs Dependencies'
+npm install &>>$LOG_FILE
+statuscheck $?
+
+echo "update systemD service file"
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE
+statuscheck $?
+
+echo 'setup ${COMPONENT} service'
+mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>$LOG_FILE
+statuscheck $?
+
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable ${COMPONENT} &>>$LOG_FILE
+
+echo "start ${COMPONENT} service"
+systemctl start ${COMPONENT} &>>$LOG_FILE
+statuscheck $?
+
+}
