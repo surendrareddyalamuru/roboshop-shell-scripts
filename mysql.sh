5a454380ea0a9db4614083bf60492e37aee0bd06
surendra reddy <surendrareddylamuru01@gmail.com>
2022-10-26 16:04:57 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -22,8 +22,8 @@ statuscheck $?
 
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
-echo 'SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
-      FLUSH PRIVILEGES;' >/tmp/root-pass.sql
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
+FLUSH PRIVILEGES;" >/tmp/root-pass.sql
 
 echo "change the default root password"
 mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
