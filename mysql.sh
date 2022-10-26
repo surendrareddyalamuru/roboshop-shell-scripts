98f5791d7b15e66080fe62fbeb8126466136e598
surendra reddy <surendrareddylamuru01@gmail.com>
2022-10-26 15:42:50 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -20,7 +20,7 @@ systemctl enable mysqld &>>$LOG_FILE
 systemctl start mysqld &>>$LOG_FILE
 statuscheck $?
 
-DEFAULT_PASSWORD=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 echo 'SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
       FLUSH PRIVILEGES;' >/tmp/root-pass.sql
