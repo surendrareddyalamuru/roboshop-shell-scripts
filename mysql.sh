8f0d5399fe4460ca4d87010a07b8ff235dfa62c3
surendra reddy <surendrareddylamuru01@gmail.com>
2022-10-27 10:21:13 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -24,7 +24,7 @@ statuscheck $?
 DEFAULT_PASSWORD=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('mypass');
-     FLUSH PRIVILEGES;"
+     FLUSH PRIVILEGES;" >/tmp/root-pass.sql
 
 # mysql_secure_installation
 
