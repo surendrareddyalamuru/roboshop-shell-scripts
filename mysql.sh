adef3b97ab0baff87850fccb91b0fe6f12779e5f
surendra reddy <surendrareddylamuru01@gmail.com>
2022-10-28 18:24:01 +0530

    mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -8,21 +8,21 @@ fi
 
 echo "Setting Up MySQL Repo"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
-StatusCheck $?
+statuscheck $?
 
 echo "Disable MySQL Default Module to Enable 5.7 MySQL"
 dnf module disable mysql -y &>>$LOG_FILE
-StatusCheck $?
+statuscheck $?
 
 echo "Install MySQL"
 yum install mysql-community-server -y &>>$LOG_FILE
-StatusCheck $?
+statuscheck $?
 
 
 echo "Start MySQL Service"
 systemctl enable mysqld &>>$LOG_FILE
 systemctl restart mysqld &>>$LOG_FILE
-StatusCheck $?
+statuscheck $?
 
 
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
@@ -34,26 +34,26 @@ echo "show databases;" |mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
 if [ $? -ne 0 ]; then
   echo "Change the default root password"
   mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
-  StatusCheck $?
+  statuscheck $?
 fi
 
 echo 'show plugins'| mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} 2>/dev/null | grep validate_password &>>$LOG_FILE
 if [ $? -eq 0 ]; then
   echo "Uninstall Password Validation Plugin"
   echo "uninstall plugin validate_password;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
-  StatusCheck $?
+ statuscheck $?
 fi
 
 echo "Download Schema"
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG_FILE
-StatusCheck $?
+statuscheck $?
 
 echo "Extract Schema"
 cd /tmp
 unzip -o mysql.zip &>>$LOG_FILE
-StatusCheck $?
+statuscheck $?
 
 echo "Load Schema"
 cd mysql-main
 mysql -u root -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql &>>$LOG_FILE
-StatusCheck $?
+statuscheck $?
